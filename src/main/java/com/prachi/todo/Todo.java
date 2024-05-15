2ebf3fc30f7dcb0c920ba727596efd5dbbb351c7
Prachi Jindal <prachibansal135@gmail.com>
2024-05-15 11:12:18 +0200

    Spring MVC - todo list

---
+++ src/main/java/com/prachi/todo/Todo.java
@@ -0,0 +1,87 @@
+package com.prachi.todo;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Todo {
+
+    private int id;
+    private String user;
+    private String desc;
+    private Date targetDate;
+    private boolean isDone;
+
+    public Todo(int id, String user, String desc, Date targetDate, boolean isDone) {
+        this.id = id;
+        this.user = user;
+        this.desc = desc;
+        this.targetDate = targetDate;
+        this.isDone = isDone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(Date targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    @Override
+    public String toString() {
+        return "ToString : Todo{" +
+                "id=" + id +
+                ", user='" + user + '\'' +
+                ", desc='" + desc + '\'' +
+                ", targetDate=" + targetDate +
+                ", isDone=" + isDone +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Todo))
+            return false;
+        Todo todo = (Todo) obj;
+        return getId() == todo.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/list-todos.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Todos!!!</title>
+</head>
+<body>
+    <H1>Welcome</H1>
+Your Todos are
+${todos}
+</body>
+</html>
