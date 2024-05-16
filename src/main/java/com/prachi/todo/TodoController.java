1c8b741c96481501ecca3966ea2565f3cc8724a3
Prachi Jindal <prachibansal135@gmail.com>
2024-05-16 15:17:31 +0200

    Spring MVC - todo list - add and delte todos - styling with bootstrap

--- src/main/java/com/prachi/todo/TodoController.java
+++ src/main/java/com/prachi/todo/TodoController.java
@@ -6,33 +6,36 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 
     @Autowired
     TodoService service;
 
     @RequestMapping(value = "/list-todos", method = RequestMethod.GET)
-    public String showTodoList(ModelMap model, HttpServletRequest request) {
-        String name = (String) request.getSession().getAttribute("name");
+    public String showTodoList(ModelMap model) {
+        String name = (String) model.get("name");
         model.addAttribute("todos", service.retrieveTodos(name));
         return "list-todos";
     }
 
     @RequestMapping(value = "/add-todos", method = RequestMethod.GET)
-    public String showTodoPage() {
+    public String showTodoPage(ModelMap model) {
+        model.addAttribute("todo", new Todo(0, "in28Minutes", "Default Description", new Date(), false ));
         return "todos";
     }
 
     @RequestMapping(value = "/add-todos", method = RequestMethod.POST)
-    public String addTodo(ModelMap model, @RequestParam String desc, HttpServletRequest request) {
-        String name = (String) request.getSession().getAttribute("name");
-        service.addTodo(name, desc, new Date(), false  );
+    public String addTodo(ModelMap model, Todo todo) {
+        String name = (String) model.get("name");
+        service.addTodo(name, todo.getDesc(), new Date(), false);
         return "redirect:list-todos";
     }
 
--- src/main/webapp/WEB-INF/view/todos.jsp
+++ src/main/webapp/WEB-INF/view/todos.jsp
@@ -1,3 +1,5 @@
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+
 <html>
 <head>
 <title>Todos for ${name}</title>
@@ -6,15 +8,15 @@
 <body>
 <div class="container">
     <H1>Add a Todos</H1>
-    <form action="/add-todos" method="post">
+    <form:form action="/add-todos" method="post" commandName="todo">
 
         <fieldset class="form-group">
-        <label>Description</label>
-        <input name="desc" type="text" class="form-control" required="requirefd"/>
+        <form:label path="desc">Description</form:label>
+        <form:input path="desc" type="text" class="form-control" required="required"></form:input>
         </fieldset>
 
         <input class ="btn btn-success" type="submit" value="Add"/>
-    </form>
+    </form:form>
 </div>
 <script src="webjars/jquery/1.9.1/jquery.min.js"></script>
 <script src="webjars/bootstrap/3.3.6/js/bootstrap.min.js"></script>
